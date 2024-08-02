98f79a920b23bfbc15b1a6bffd53c7a998d1c059
Auto Updater <>
2024-08-03 02:44:15 +0300

    update CitiesContext.jsx

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -86,7 +86,7 @@ function CitiesProvider({ children }) {
       dispatch({ type: "loading" });
 
       try {
-        const res = await fetch(`${BASE_URL}/?id=${id}`);
+        const res = await fetch(`${BASE_URL}/cities/?id=${id}`);
         const data = await res.json();
         dispatch({ type: "city/loaded", payload: data[0] });
       } catch {
@@ -103,7 +103,7 @@ function CitiesProvider({ children }) {
     dispatch({ type: "loading" });
 
     try {
-      const res = await fetch(`${BASE_URL}`, {
+      const res = await fetch(`${BASE_URL}/cities/`, {
         method: "POST",
         body: JSON.stringify(newCity),
         headers: { "Content-Type": "application/json" },
